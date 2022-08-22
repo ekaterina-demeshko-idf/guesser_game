bf394464cc32ceca33ab60ee8eadd135994eb4a3
ekaterina-demeshko-idf <ekaterina.demeshko@idfinance.com>
2022-08-22 13:35:56 +0300

    fix magic numbers in generate number usecase

--- packages/domain/lib/usecase/generate_number_usecase.dart
+++ packages/domain/lib/usecase/generate_number_usecase.dart
@@ -1,11 +1,14 @@
 import 'dart:math';
 import 'usecase.dart';
+const int maxNumber = 9;
+const int startNumber = 1;
 
 class GenerateNumberUsecase implements UseCase<int> {
   Random random = Random();
 
+
   @override
   int call() {
-    return random.nextInt(9) + 1;
+    return random.nextInt(maxNumber) + startNumber;
   }
 }
