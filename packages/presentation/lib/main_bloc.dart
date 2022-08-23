74dfb55298652c79ca134415c4884c7d19f5ecb3
ekaterina-demeshko-idf <ekaterina.demeshko@idfinance.com>
2022-08-23 11:30:03 +0300

    fix linter warnings

--- packages/presentation/lib/main_bloc.dart
+++ packages/presentation/lib/main_bloc.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'package:bloc/bloc.dart';
 import 'package:domain/usecase/check_number_usecase.dart';
 import 'package:domain/usecase/generate_number_usecase.dart';
--- packages/presentation/lib/main_state.dart
+++ packages/presentation/lib/main_state.dart
@@ -5,14 +5,12 @@ class MainInitial extends MainState {
   final int randomNumber = 0;
   final bool isButtonDisabled = true;
 
-  @override
   List<Object?> get props => [counter, randomNumber, isButtonDisabled];
 }
 
 class NewGameState extends MainState {
   final int counter = 0;
 
-  @override
   List<Object?> get props => [counter];
 }
 
@@ -20,14 +18,12 @@ class YouWonState extends MainState {
   final String result = 'You Won!';
   final bool isButtonDisabled = true;
 
-  @override
   List<Object?> get props => [result, isButtonDisabled];
 }
 
 class WrongState extends MainState {
   final String result = 'Wrong!';
 
-  @override
   List<Object?> get props => [result];
 }
 
@@ -35,6 +31,5 @@ class OutOfAttemptsState extends MainState {
   final String result = 'You out of 3 free attempts. Please start a new game.';
   final bool isButtonDisabled = true;
 
-  @override
   List<Object?> get props => [result, isButtonDisabled];
 }
