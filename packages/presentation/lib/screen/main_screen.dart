b562a502e09da6b06765e9f26f8107c9490e83c2
ekaterina-demeshko-idf <ekaterina.demeshko@idfinance.com>
2022-08-25 18:02:52 +0300

    fix generateRandom call

--- packages/presentation/lib/screen/main_screen.dart
+++ packages/presentation/lib/screen/main_screen.dart
@@ -101,7 +101,7 @@ class _MainScreenState extends BlocScreenState<MainScreen, MainBloc> {
                             width: 30,
                           ),
                           ElevatedButton(
-                            onPressed: bloc.generateRandomNumber(),
+                            onPressed: () {bloc.generateRandomNumber();},
                             child: const Text(
                               'New Game',
                               style: TextStyle(fontSize: 20),
